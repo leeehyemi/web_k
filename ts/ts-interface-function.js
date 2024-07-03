712e56c101cdddb9efb2bef873b0aa64edbc221e
leehyemi11 <leehyemi11@gmail.com>
2024-07-03 23:17:59 +0900

    change

---
+++ ts/ts-interface-function.js
@@ -0,0 +1,25 @@
+var iPhoneX = {
+    marker: 'Apple',
+    name: 'iPhone X',
+    battery: 65,
+    created_at: new Date(2017),
+    features: ['스피커', '카메라', 'FaceID']
+};
+var galaxyS10 = {
+    marker: 'Samsung',
+    name: 'galaxyS10',
+    battery: 40,
+    created_at: new Date(2019),
+    features: ['카메라', '5G', '지문인식']
+};
+console.log(iPhoneX);
+console.log(galaxyS10);
+var checkBusTime = function (bus) {
+    //  버스 시간 확인 코드 지정
+    return new Date();
+};
+var testFunction = function (isBoolean, isSentence) {
+    return {
+        result: 'completed'
+    };
+};
